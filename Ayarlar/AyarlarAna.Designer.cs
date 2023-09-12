2455e7cb1d2026eeca913ccb88729a316880146d
merenisler <142229251+merenisler@users.noreply.github.com>
2023-09-12 19:22:14 +0300

    Add files via upload

---
+++ Ayarlar/AyarlarAna.Designer.cs
@@ -0,0 +1,146 @@
+﻿namespace PizzaciSon1.Ayarlar
+{
+    partial class AyarlarAna
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(AyarlarAna));
+            bunifuElipse1 = new Bunifu.Framework.UI.BunifuElipse(components);
+            btnUrunEkleKaldir = new Button();
+            ımageList3 = new ImageList(components);
+            btnUrunDuzenle = new Button();
+            btnMasaEkleKaldir = new Button();
+            btnCikis = new Button();
+            SuspendLayout();
+            // 
+            // bunifuElipse1
+            // 
+            bunifuElipse1.ElipseRadius = 5;
+            bunifuElipse1.TargetControl = this;
+            // 
+            // btnUrunEkleKaldir
+            // 
+            btnUrunEkleKaldir.BackColor = Color.MediumPurple;
+            btnUrunEkleKaldir.FlatAppearance.BorderSize = 0;
+            btnUrunEkleKaldir.FlatStyle = FlatStyle.Flat;
+            btnUrunEkleKaldir.Font = new Font("Microsoft Sans Serif", 27.75F, FontStyle.Regular, GraphicsUnit.Point);
+            btnUrunEkleKaldir.ForeColor = Color.Black;
+            btnUrunEkleKaldir.ImageAlign = ContentAlignment.MiddleLeft;
+            btnUrunEkleKaldir.ImageIndex = 0;
+            btnUrunEkleKaldir.Location = new Point(182, 165);
+            btnUrunEkleKaldir.Name = "btnUrunEkleKaldir";
+            btnUrunEkleKaldir.Size = new Size(370, 140);
+            btnUrunEkleKaldir.TabIndex = 44;
+            btnUrunEkleKaldir.Text = "Ürün Ekle/Kaldır";
+            btnUrunEkleKaldir.UseVisualStyleBackColor = false;
+            btnUrunEkleKaldir.Click += btnUrunEkleKaldir_Click;
+            // 
+            // ımageList3
+            // 
+            ımageList3.ColorDepth = ColorDepth.Depth32Bit;
+            ımageList3.ImageStream = (ImageListStreamer)resources.GetObject("ımageList3.ImageStream");
+            ımageList3.TransparentColor = Color.Transparent;
+            ımageList3.Images.SetKeyName(0, "x-g66b252b67_640.png");
+            // 
+            // btnUrunDuzenle
+            // 
+            btnUrunDuzenle.BackColor = Color.MediumPurple;
+            btnUrunDuzenle.FlatAppearance.BorderSize = 0;
+            btnUrunDuzenle.FlatStyle = FlatStyle.Flat;
+            btnUrunDuzenle.Font = new Font("Microsoft Sans Serif", 27.75F, FontStyle.Regular, GraphicsUnit.Point);
+            btnUrunDuzenle.ForeColor = Color.Black;
+            btnUrunDuzenle.ImageAlign = ContentAlignment.MiddleLeft;
+            btnUrunDuzenle.ImageIndex = 0;
+            btnUrunDuzenle.Location = new Point(733, 165);
+            btnUrunDuzenle.Name = "btnUrunDuzenle";
+            btnUrunDuzenle.Size = new Size(370, 140);
+            btnUrunDuzenle.TabIndex = 48;
+            btnUrunDuzenle.Text = "Ürün Düzenle";
+            btnUrunDuzenle.UseVisualStyleBackColor = false;
+            btnUrunDuzenle.Click += btnUrunDuzenle_Click;
+            // 
+            // btnMasaEkleKaldir
+            // 
+            btnMasaEkleKaldir.BackColor = Color.MediumPurple;
+            btnMasaEkleKaldir.FlatAppearance.BorderSize = 0;
+            btnMasaEkleKaldir.FlatStyle = FlatStyle.Flat;
+            btnMasaEkleKaldir.Font = new Font("Microsoft Sans Serif", 27.75F, FontStyle.Regular, GraphicsUnit.Point);
+            btnMasaEkleKaldir.ForeColor = Color.Black;
+            btnMasaEkleKaldir.ImageAlign = ContentAlignment.MiddleLeft;
+            btnMasaEkleKaldir.ImageIndex = 0;
+            btnMasaEkleKaldir.Location = new Point(452, 333);
+            btnMasaEkleKaldir.Name = "btnMasaEkleKaldir";
+            btnMasaEkleKaldir.Size = new Size(370, 140);
+            btnMasaEkleKaldir.TabIndex = 46;
+            btnMasaEkleKaldir.Text = "Masa Ekle/Kaldır";
+            btnMasaEkleKaldir.UseVisualStyleBackColor = false;
+            btnMasaEkleKaldir.Click += btnMasaEkleKaldir_Click;
+            // 
+            // btnCikis
+            // 
+            btnCikis.BackColor = Color.Red;
+            btnCikis.FlatAppearance.BorderSize = 0;
+            btnCikis.FlatStyle = FlatStyle.Flat;
+            btnCikis.Font = new Font("Microsoft Sans Serif", 12.75F, FontStyle.Regular, GraphicsUnit.Point);
+            btnCikis.ForeColor = Color.Black;
+            btnCikis.ImageIndex = 0;
+            btnCikis.ImageList = ımageList3;
+            btnCikis.Location = new Point(1232, 9);
+            btnCikis.Name = "btnCikis";
+            btnCikis.Size = new Size(46, 43);
+            btnCikis.TabIndex = 45;
+            btnCikis.UseVisualStyleBackColor = false;
+            btnCikis.Click += btnCikis_Click;
+            // 
+            // AyarlarAna
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = SystemColors.ControlDarkDark;
+            ClientSize = new Size(1285, 635);
+            Controls.Add(btnUrunEkleKaldir);
+            Controls.Add(btnUrunDuzenle);
+            Controls.Add(btnMasaEkleKaldir);
+            Controls.Add(btnCikis);
+            FormBorderStyle = FormBorderStyle.None;
+            Name = "AyarlarAna";
+            StartPosition = FormStartPosition.CenterScreen;
+            Text = "AyarlarAna";
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private Bunifu.Framework.UI.BunifuElipse bunifuElipse1;
+        private Button btnUrunEkleKaldir;
+        private Button btnUrunDuzenle;
+        private Button btnMasaEkleKaldir;
+        private Button btnCikis;
+        private ImageList ımageList3;
+    }
+}
---
+++ Ayarlar/MasaEkleKaldir.cs
@@ -0,0 +1,199 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PizzaciSon1.Ayarlar
+{
+    public partial class MasaEkleKaldir : Form
+    {
+        public MasaEkleKaldir()
+        {
+            InitializeComponent();
+        }
+
+        public static string ConnectionString = "Data Source=.;Initial Catalog = Pizzacı; Integrated Security = True";
+        int masaNo = 0;
+        string masaYeri = "";
+
+        private void btnListele_Click(object sender, EventArgs e)
+        {
+            lstViewMasalar.Items.Clear();
+            string masaYeriSecim = "";
+            masaYeriSecim = cmbBxMasaYeri.Text;
+            SqlConnection bg = new SqlConnection(ConnectionString);
+            bg.Open();
+            SqlCommand cmd = new SqlCommand("select * from Masalar where masaYeri='" + masaYeriSecim + "' order by masaNo", bg);
+            SqlDataReader oku = cmd.ExecuteReader();
+            while (oku.Read())
+            {
+                ListViewItem listele = new ListViewItem();
+                listele.Text = oku["masaNo"].ToString();
+                listele.SubItems.Add(oku["masaYeri"].ToString());
+                lstViewMasalar.Items.Add(listele);
+            }
+            bg.Close();
+
+            int masaAdet = 0;
+            bg.Open();
+            cmd = new SqlCommand("select Count(*) as masaAdet from Masalar where masaYeri='" + masaYeriSecim + "'", bg);
+            oku = cmd.ExecuteReader();
+            while (oku.Read())
+            {
+                masaAdet = (int)oku["masaAdet"];
+            }
+            bg.Close();
+            masaAdet += 1;
+            cmbBxMasaNo.Text = masaAdet.ToString();
+            cmbBxMasaYeri2.Text = masaYeriSecim;
+        }
+
+        private void lstViewMasalar_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            string items = "";
+            if (lstViewMasalar.SelectedItems.Count == 1)
+            {
+                for (int i = 0; i < 2; i++)
+                {
+                    items = lstViewMasalar.SelectedItems[0].SubItems[i].Text;
+                    if (i == 0)
+                        masaNo = Convert.ToInt32(items);
+                    if (i == 1)
+                        masaYeri = items;
+                }
+            }
+        }
+
+        private void btnKaldir_Click(object sender, EventArgs e)
+        {
+            SqlConnection bg = new SqlConnection(ConnectionString);
+            bg.Open();
+            SqlCommand guncel = new SqlCommand("delete Masalar where masaNo=@masaNo and masaYeri=@masaYeri", bg);
+            guncel.Parameters.AddWithValue("@masaNo", masaNo);
+            guncel.Parameters.AddWithValue("@masaYeri", masaYeri);
+            guncel.ExecuteNonQuery();
+            bg.Close();
+
+            bg.Open();
+            guncel = new SqlCommand("delete ListboxKontrol where masaNo=@masaNo and masaYeri=@masaYeri", bg);
+            guncel.Parameters.AddWithValue("@masaNo", masaNo);
+            guncel.Parameters.AddWithValue("@masaYeri", masaYeri);
+            guncel.ExecuteNonQuery();
+            bg.Close();
+
+            if (masaYeri=="Bahçe")
+                masaYeri = "Bahce";
+
+            string masa = masaYeri + masaNo;
+            bg.Open();
+            SqlCommand cmd = new SqlCommand("drop table " + masa + "", bg);
+            cmd.ExecuteNonQuery();
+            bg.Close();
+
+            string masaYeriSecim = "";
+            lstViewMasalar.Items.Clear();
+            masaYeriSecim = cmbBxMasaYeri2.Text;
+            bg.Open();
+            cmd = new SqlCommand("select * from Masalar where masaYeri='" + masaYeriSecim + "' order by masaNo", bg);
+            SqlDataReader oku = cmd.ExecuteReader();
+            while (oku.Read())
+            {
+                ListViewItem listele = new ListViewItem();
+                listele.Text = oku["masaNo"].ToString();
+                listele.SubItems.Add(oku["masaYeri"].ToString());
+                lstViewMasalar.Items.Add(listele);
+            }
+            bg.Close();
+
+            int masaAdet = 0;
+            bg.Open();
+            cmd = new SqlCommand("select Count(*) as masaAdet from Masalar where masaYeri='" + masaYeriSecim + "'", bg);
+            oku = cmd.ExecuteReader();
+            while (oku.Read())
+            {
+                masaAdet = (int)oku["masaAdet"];
+            }
+            bg.Close();
+            masaAdet += 1;
+            cmbBxMasaNo.Text = masaAdet.ToString();
+        }
+
+        private void btnEkle_Click(object sender, EventArgs e)
+        {
+            string yeniMasaYeri = "";
+            int yeniMasaNo = 0;
+
+            if (cmbBxMasaYeri2.Text != "")
+                yeniMasaYeri = cmbBxMasaYeri2.Text;
+            if (cmbBxMasaNo.Text != "")
+                yeniMasaNo = Convert.ToInt32(cmbBxMasaNo.Text);
+
+            SqlConnection bg = new SqlConnection(ConnectionString);
+            if (yeniMasaYeri != "" && yeniMasaNo != 0)
+            {
+                bg.Open();
+                SqlCommand guncel = new SqlCommand("insert into Masalar(masaNo, masaYeri, durum, fiyat) values(@masaNo, @masaYeri, 'false', 0)", bg);
+                guncel.Parameters.AddWithValue("@masaNo", yeniMasaNo);
+                guncel.Parameters.AddWithValue("@masaYeri", yeniMasaYeri);
+                guncel.ExecuteNonQuery();
+                bg.Close();
+
+                bg.Open();
+                guncel = new SqlCommand("insert into ListboxKontrol(listbox, tutar, durum, masaYeri, masaNo, fisNotu) values('', 0, 'false', @masaYeri, @masaNo, '')", bg);
+                guncel.Parameters.AddWithValue("@masaNo", yeniMasaNo);
+                guncel.Parameters.AddWithValue("@masaYeri", yeniMasaYeri);
+                guncel.ExecuteNonQuery();
+                bg.Close();
+
+                if (yeniMasaYeri=="Bahçe")
+                    yeniMasaYeri = "Bahce";
+                string masa = yeniMasaYeri + yeniMasaNo;
+                bg.Open();
+                guncel = new SqlCommand("create table " + masa + "(yazdir bit, adet int, isim nvarchar(50), fiyat int, stokKategori nvarchar(30), kategori nvarchar(10));", bg);
+                guncel.ExecuteNonQuery();
+                bg.Close();
+
+                lstViewMasalar.Items.Clear();
+                string masaYeriSecim = "";
+                masaYeriSecim = cmbBxMasaYeri2.Text;
+                bg.Open();
+                SqlCommand cmd = new SqlCommand("select * from Masalar where masaYeri='" + masaYeriSecim + "' order by masaNo", bg);
+                SqlDataReader oku = cmd.ExecuteReader();
+                while (oku.Read())
+                {
+                    ListViewItem listele = new ListViewItem();
+                    listele.Text = oku["masaNo"].ToString();
+                    listele.SubItems.Add(oku["masaYeri"].ToString());
+                    lstViewMasalar.Items.Add(listele);
+                }
+                bg.Close();
+                cmbBxMasaNo.Text = (yeniMasaNo + 1).ToString();
+            }
+        }
+        
+        private void btnCikis_Click(object sender, EventArgs e)
+        {
+            AyarlarAna ayarlarAna = new AyarlarAna();
+            ayarlarAna.Show();
+            this.Hide();
+        }
+        private void btnUrunDuzenle_Click(object sender, EventArgs e)
+        {
+            UrunDuzenle urunDuzenle = new UrunDuzenle();
+            urunDuzenle.Show();
+            this.Hide();
+        }
+        private void btnUrunEkleKaldir_Click(object sender, EventArgs e)
+        {
+            UrunEkleKaldir urunEkleKaldir = new UrunEkleKaldir();
+            urunEkleKaldir.Show();
+            this.Hide();
+        }
+    }
+}
